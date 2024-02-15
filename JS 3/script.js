3434a4e6e0b8e15c013471625dcc248668101ea8
bayel0055 <156521556+bayel0055@users.noreply.github.com>
2024-02-15 21:43:04 +0600

    Add files via upload

---
+++ JS 3/script.js
@@ -0,0 +1,305 @@
+const array = [
+    {
+        name: "bayel",
+        age: 17,
+        adres: "anar",
+        tipe: 'man',
+        id: 1,
+
+        hobbies: {
+            sport: {
+                can: {
+                    1: "jiujitsu",
+                    2: "football",
+                    3: "box",
+                    4: "karate",
+                    5: "basketball",
+                    6: "hokei",
+                    7: "voleyball",
+                    8: "tenis",
+                    9: "gimnastika",
+                }
+
+            },
+            cook: {
+                food: {
+                    1: "manty",
+                    2: "plov",
+                    3: "ash",
+                    4: "kotlety",
+                    5: "shorpo",
+                    6: "mastava",
+                    7: "goroh",
+                    8: "shoolya",
+                    9: "sup",
+
+                }
+            }
+
+        },
+        family: {
+            mama: {
+                name: "dilya",
+                age: "40",
+                adres: "zapad",
+                gender: 'women',
+                id: 2,
+                work: "biznes"
+            },
+            dad: {
+                name: "tilek",
+                age: "44",
+                adres: "zpd",
+                gender: 'man',
+                id: 3,
+            },
+            sister: {
+                name: "damira",
+                age: "16",
+                adres: "selo",
+                gender: 'women',
+                id: 4,
+            },
+            sister2: {
+                name: "rayana",
+                age: "8",
+                adres: "selo1",
+                gender: 'women',
+                id: 5,
+            },
+            sister3: {
+                name: "adelina",
+                age: "7",
+                adres: "selo2",
+                gender: 'women',
+                id: 6,
+            },
+            Friend: {
+                name: "muha",
+                age: "17",
+                adres: "dom",
+                gender: 'men',
+                id: 7,
+            },
+            Friend1: {
+                name: "nurs",
+                age: "18",
+                adres: "selo31",
+                gender: 'men',
+                id: 8,
+                hobbies: {
+                    sport: "football"
+                },
+                study: "makar"
+            },
+            Friend2: {
+                name: "askar",
+                age: "20",
+                adres: "selo13",
+                gender: 'men',
+                id: 9,
+
+            },
+            girlfrend: {
+                name: "amina",
+                age: "17",
+                adres: "korgon",
+                gender: 'women',
+                id: 10,
+                family: {
+                    mother: {
+                        name: "Кумушай",
+                        age: "56",
+                        adres: "zapad",
+                        gender: "women",
+                        id: 11
+                    },
+                    brother: {
+                        name: "islam",
+                        age: "32",
+                        adres: "fr tilek",
+                        gender: 'men',
+                        id: 12,
+                    },
+                    sister: {
+                        name: "gulmairam",
+                        age: "37",
+                        adres: "korea",
+                        gender: 'women',
+                        id: 13,
+                    },
+                    sister: {
+                        name: "guldana",
+                        age: "34",
+                        adres: "usa",
+                        gender: 'women',
+                        id: 14,
+                    },
+                    sister: {
+                        name: "marica",
+                        age: "34",
+                        adres: "ikutia",
+                        gender: 'women',
+                        id: 15,
+                    },
+                    cousin: {
+                        name: "muham",
+                        age: "16",
+                        adres: "bokonbaeva",
+                        gender: 'men',
+                        id: 16,
+                    },
+                    cousin: {
+                        name: "kuma",
+                        age: "24",
+                        adres: "alay",
+                        gender: 'women',
+                        id: 17,
+                    }
+                }
+            },
+
+
+
+        },
+        bjj: {
+            couch: {
+                name: "urmat",
+                age: "20",
+                adres: "alay",
+                gender: 'men',
+                id: 18,
+            },
+            atleth: {
+                name: "arslan",
+                age: "15",
+                adres: "ozgon",
+                gender: 'men',
+                id: 19,
+            },
+            atleth2:{
+                name: "arsen",
+                age: "15",
+                adres: "tak",
+                gender: 'men',
+                id: 20,
+            },
+            atleth3:{
+                name: "jumadyl",
+                age: "17",
+                adres: "alay",
+                gender: 'men',
+                id: 21,
+            },
+            atleth4:{
+                name: "chera",
+                age: "14",
+                adres: "ozgon",
+                gender: 'men',
+                id: 22,
+            },
+            atleth5:{
+                name: "sary",
+                age: "15",
+                adres: "reg",
+                gender: 'men',
+                id: 23,
+            },
+            atleth6:{
+                name: "twelf",
+                age: "12",
+                adres: "adyr",
+                gender: 'men',
+                id: 24,
+            },
+            atleth7:{
+                name: "islam",
+                age: "15",
+                adres: "talas",
+                gender: 'men',
+                id: 25,
+            },
+            atleth8:{
+                name: "krash",
+                age: "16",
+                adres: "transilvania",
+                gender: 'men',
+                id: 26,
+            },
+        },
+        cours:{
+            teacher:{
+                name: "kishimzhan",
+                age: "38",
+                adres: "toloikon",
+                gender: 'women',
+                id: 27,
+            },
+            student:{
+                name: "iris",
+                age: "55",
+                adres: "trend",
+                gender: 'women',
+                id: 91,
+            },
+            student:{
+                name: "nuris",
+                age: "66",
+                adres: "batken",
+                gender: 'men',
+                id: 92,
+            },
+            student:{
+                name: "dic",
+                age: "76",
+                adres: "yeshka",
+                gender: 'women',
+                id: 93,
+            },
+            student:{
+                name: "paula",
+                age: "44",
+                adres: "india",
+                gender: 'women',
+                id: 94,
+            },
+            student:{
+                name: "shegol",
+                age: "77",
+                adres: "uzgen",
+                gender: 'men',
+                id: 95,
+            },
+            student:{
+                name: "aktan",
+                age: "55",
+                adres: "kuldja",
+                gender: 'men',
+                id: 96,
+            },
+            student:{
+                name: "kanykey",
+                age: "19",
+                adres: "silv",
+                gender: 'women',
+                id: 97,
+            },
+            student:{
+                name: "usman",
+                age: "167",
+                adres: "vania",
+                gender: 'men',
+                id: 98,
+            },
+            student:{
+                name: "aidar",
+                age: "11",
+                adres: "tran",
+                gender: 'men',
+                id: 99,
+            }
+        }
+
+    },
+
+]
